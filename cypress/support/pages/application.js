ccaca80ca9cdc96cbb7d4d4097437dc99ba1cde9
adelinop <adelinopessanha@hotmail.com>
2023-01-09 18:18:25 +0000

    adding test AUR-13 Brkoen Images

--- cypress/support/pages/application.js
+++ cypress/support/pages/application.js
@@ -5,6 +5,7 @@ import HomePage from './applications/homePage'
 import AbPage from './applications/abPage'
 import AddRemoveElementsPage from './applications/addRemoveElementsPage'
 import BasicAuthPage from './applications/basicAuthPage'
+import BrokenImagesPage from './applications/brokenImagesPage'
 /**
  * This is the main class the encapsulates all pages regarding the application (Except base pages since they are not meant to be directly called).
  *
@@ -22,6 +23,7 @@ class Application {
 		this.abPage = new AbPage()
 		this.addRemoveElementsPage = new AddRemoveElementsPage()
 		this.basicAuthPage = new BasicAuthPage()
+		this.brokenImagesPage = new BrokenImagesPage()
 	}
 }
 
